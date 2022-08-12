165d06e8e6367a8718e46934cad92df2dbb1f836
vanderms <vanderms.84@gmail.com>
2022-08-12 09:23:53 -0300

    feat: add colors schema, screens and main typography to project styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,23 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    screens: {
+      sm: '20.4375rem',
+      md: '45rem',
+      xl: '74rem',
+    },
+
+    colors: {
+      "transparent": "transparent",
+      "neutral": "#fff",
+      "neutral-100": "#EEEFF4",
+      "neutral-200": "#C8CCD8",
+      "neutral-400": "#7D828F",
+      "neutral-500": "#60636D",
+      "neutral-800": "#1B1D23",
+      "danger": "#DF5656"
+    },
+
     extend: {},
   },
   plugins: [],
