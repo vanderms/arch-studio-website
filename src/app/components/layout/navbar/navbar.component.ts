1d7c22319d42fbd124b4f961e4af2d914a45035c
vanderms <vanderms.84@gmail.com>
2022-08-12 12:18:06 -0300

    feat: implement responsive navbar

--- src/app/components/layout/navbar/navbar.component.ts
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -1,16 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+function getPage(url: string): string {
+  switch (url) {
+    case '/about':
+      return 'ABOUT';
+    case '/portfolio':
+      return 'PORTFOLIO';
+    case '/contact':
+      return 'CONTACT';
+  }
+  return 'HOME';
+}
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class NavbarComponent implements OnInit {
+  sidebarOpen: boolean = false;
+  navbarFixed: boolean = false;
+  page: string = '';
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+    this.router.events.subscribe(() => {
+      this.sidebarOpen = false;
+      this.page = getPage(this.router.url);
+      console.log(this.page);
+    });
   }
 
+  toggleSidebar(): void {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
+  closeSidebar(): void {
+    this.sidebarOpen = false;
+  }
+
+  @HostListener('window:scroll', ['$event.target'])
+  onScroll(html: Document) {
+    let scroll: number = html.scrollingElement?.scrollTop ?? 0;
+    this.navbarFixed = scroll > 96;
+  }
 }
